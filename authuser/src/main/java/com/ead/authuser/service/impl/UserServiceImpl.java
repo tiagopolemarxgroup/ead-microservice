f0a5ec9ef3b2f307354929b4a52c52029b7dd909
Tiago Dos Santos <63621436+tiagopolemarxgroup@users.noreply.github.com>
2024-02-05 00:02:10 -0300

    Spring Security
    Foi adicionado a parte autenticação com Spring Security

---
+++ authuser/src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ead.authuser.service.impl;
+
+import com.ead.authuser.repository.UserRepository;
+import com.ead.authuser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
